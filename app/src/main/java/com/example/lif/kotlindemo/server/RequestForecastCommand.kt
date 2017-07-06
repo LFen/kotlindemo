2e80335d10876b81662ced7f47599f218bde2767
lif <lif@icloudoor.com>
2017-07-06 15:07:53 +0800

    reformat code

--- app/src/main/java/com/example/lif/kotlindemo/server/RequestForecastCommand.kt
+++ app/src/main/java/com/example/lif/kotlindemo/server/RequestForecastCommand.kt
@@ -1,12 +1,13 @@
 package com.example.lif.kotlindemo.server
 
+import com.example.lif.kotlindemo.domain.Command
 import com.example.lif.kotlindemo.domain.DomainClasses
 
 /**
  * Created by lif on 2017/7/6.
  */
 
-class RequestForecastCommand(val zipCode: String): com.example.lif.kotlindemo.domain.Command<DomainClasses.ForecastList> {
+class RequestForecastCommand(val zipCode: String): Command<DomainClasses.ForecastList> {
 
     override fun execute(): com.example.lif.kotlindemo.domain.DomainClasses.ForecastList {
         val forecastRequest = ForecastRequest(zipCode)
